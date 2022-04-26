2f79ebacaa8c9377fdb188241d37aa496b49feab
fletchjeff <fletch.jeff@gmail.com>
2022-04-26 13:17:03 +0200

    DAG update

--- dags/hello_world_dag.py
+++ dags/hello_world_dag.py
@@ -12,8 +12,8 @@ args = {
     'retries': 0
 }
 
-response = requests.get("http://192.168.1.101:5000/api")
-dag_list = json.loads(response.text)
+#response = requests.get("http://192.168.1.101:5000/api")
+#dag_list = json.loads(response.text)
 
 def print_hello():
     return 'Hello world from first Airflow DAG!'
@@ -31,13 +31,13 @@ dag = DAG('hello_world_2', description='Hello World DAG',
 
 with dag:        
     hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
-    for dag_number in dag_list:
-        op = PythonOperator(
-            task_id=f'dag_number_{dag_number}',
-            python_callable=api_dag,
-            op_kwargs={'dag_list_number': dag_number}
-        )
-        hello_operator >> op
+    # for dag_number in dag_list:
+    #     op = PythonOperator(
+    #         task_id=f'dag_number_{dag_number}',
+    #         python_callable=api_dag,
+    #         op_kwargs={'dag_list_number': dag_number}
+    #     )
+    #     hello_operator >> op
 
 
 hello_operator
