445d59981f6116dce355e49c5c22e359b2bd6e97
Jeff Fletcher <fletch.jeff@gmail.com>
2022-10-04 10:55:15 +0200

    GH Update

--- dags/hello_world_dag.py
+++ dags/hello_world_dag.py
@@ -12,13 +12,9 @@ args = {
 def print_hello():
     return 'Hello world from first Airflow DAG!'
 
-def api_dag(dag_list_number):
-    print("testing the dag")
-    return dag_list_number
-
 dag = DAG('hello_world_gh', description='Hello World DAG',
-          schedule_interval='0 12 * * *',
-          start_date=datetime(2017, 3, 20), 
+          schedule_interval=None,
+          start_date=datetime(2022, 3, 20), 
           catchup=False,
           default_args=args
         )
